7ec7b5fbe986a08d55c37db1382b69915c54d291
Samson Nwokike <nwokikesamson@gmail.com>
2024-08-14 02:53:08 +0100

    Refactor YoutubePlayer component to use Loader component and handle error

--- src/components/YoutubePlayer.tsx
+++ src/components/YoutubePlayer.tsx
@@ -1,17 +1,28 @@
 import ReactPlayer from 'react-player'
+import Loader from './Loader';
 
 interface YoutubePlayerProps {
   videoKey: string;
+  error?: string
+  loading?: boolean
 }
 
-const YoutubePlayer = ({ videoKey }: YoutubePlayerProps) => (
-  <ReactPlayer
-    className="video-player"
-    url={`https://www.youtube.com/watch?v=${videoKey}`}
-    controls={true}
-    playing={true}
-    data-testid="youtube-player"
-  />
-);
+const YoutubePlayer = ({ videoKey, loading, error }: YoutubePlayerProps) => {
+  if (loading) return <Loader />
+  if (error) return <h6>Error loading trailer</h6>
+  return (
+    videoKey ?
+      <div>
+        <ReactPlayer
+          className="video-player"
+          url={`https://www.youtube.com/watch?v=${videoKey}`}
+          controls={true}
+          playing={true}
+          data-testid="youtube-player"
+        />
+      </div>
+      : <div style={{ padding: "30px" }}><h6>no trailer available. Try another movie</h6></div>
+  );
+}
 
 export default YoutubePlayer;
