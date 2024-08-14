da9aef2fe222f4cdadb0d9966c6f01d15195fddd
Samson Nwokike <nwokikesamson@gmail.com>
2024-08-14 02:39:01 +0100

    Refactored Starred component

--- src/components/Starred.tsx
+++ src/components/Starred.tsx
@@ -1,49 +1,57 @@
-import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import starredSlice from '../data/starredSlice'
 import Movie from './Movie'
 import { IMovie } from '../types/movie'
-import { IRootState } from '../data/store'
-import '../styles/starred.scss'
+import { useAppSelector } from '../hooks/useAppSelector'
+import { useAppDispatch } from '../hooks/useAppDispatch'
 
+import '../styles/starred.scss'
 
 interface StarredProps {
   viewTrailer: (movie: IMovie) => void
 }
 
-const Starred = ({viewTrailer}: StarredProps) => {
+const Starred = ({ viewTrailer }: StarredProps) => {
+  const { starred } = useAppSelector((state) => state);
+  const dispatch = useAppDispatch();
 
-  const state = useSelector((state: IRootState) => state)
-  const { starred } = state
-  const { clearAllStarred } = starredSlice.actions
-  const dispatch = useDispatch()
+  const handleClearAllStarred = () => {
+    dispatch(starredSlice.actions.clearAllStarred());
+  };
 
   return (
     <div className="starred" data-testid="starred">
-      {starred.starredMovies.length > 0 && (<div data-testid="starred-movies" className="starred-movies">
-        <h6 className="header">Starred movies</h6>
-        <div className="row">
-          {starred.starredMovies.map((movie) => (
-            <Movie
-              movie={movie}
-              key={movie.id}
-              viewTrailer={viewTrailer}
-            />
-          ))}
+      {starred.starredMovies.length > 0 && (
+        <div data-testid="starred-movies" className="starred-movies">
+          <h6 className="header">Starred Movies</h6>
+          <div className="row">
+            {starred.starredMovies.map((movie) => (
+              <Movie key={movie.id} movie={movie} viewTrailer={viewTrailer} />
+            ))}
+          </div>
+
+          <footer className="text-center">
+            <button
+              className="btn btn-primary"
+              onClick={handleClearAllStarred}
+            >
+              Remove All Starred
+            </button>
+          </footer>
         </div>
+      )}
 
-        <footer className="text-center">
-          <button className="btn btn-primary" onClick={() => dispatch(clearAllStarred())}>Remove all starred</button>
-        </footer>
-      </div>)}
-
-      {starred.starredMovies.length === 0 && (<div className="text-center empty-cart">
-        <i className="bi bi-star" />
-        <p>There are no starred movies.</p>
-        <p>Go to <Link to='/'>Home</Link></p>
-      </div>)}
+      {starred.starredMovies.length === 0 && (
+        <div className="text-center empty-cart">
+          <i className="bi bi-star" />
+          <p>There are no starred movies.</p>
+          <p>
+            Go to <Link to="/">Home</Link>
+          </p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Starred
