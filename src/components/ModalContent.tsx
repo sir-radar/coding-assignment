13acdac1513b7f6095ddff1a12376d7b3853c305
Samson Nwokike <nwokikesamson@gmail.com>
2024-08-15 01:26:49 +0100

    Added  unit tests for Modal component

--- src/components/ModalContent.tsx
+++ src/components/ModalContent.tsx
@@ -7,10 +7,10 @@ interface ModalContentProps {
 
 export default function ModalContent({ children, closeModal }: ModalContentProps) {
   return (
-    <div className="modal">
+    <div data-testid="modal-body" className="modal">
       <div className="overlay" onClick={() =>  closeModal()}></div>
       <div className="modal-content">
-        <button type="button" className="close" onClick={() => closeModal()} aria-label="Close">
+        <button data-testid="modal-close-btn" type="button" className="close" onClick={() => closeModal()} aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
         {children}
---
+++ src/test/Modal.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Modal from '../components/Modal';
+
+describe('Modal', () => {
+  it('should hide the scroll when showContent is true', () => {
+    render(<Modal showContent={true} closeModal={() => {}} children={<div>Modal Content</div>} />);
+    expect(document.body.style.overflow).toBe('hidden');
+  });
+
+  it('should show the modal content when showContent is true', () => {
+    render(<Modal showContent={true} closeModal={() => {}} children={<div>Modal Content</div>} />);
+    expect(screen.getByTestId('modal-body')).toBeInTheDocument();
+  });
+
+  it('should not show the modal content when showContent is false', () => {
+    render(<Modal showContent={false} closeModal={() => {}} children={<div>Modal Content</div>} />);
+    expect(screen.queryByText('modal-body')).not.toBeInTheDocument();
+  });
+
+  it('should call closeModal when the close button is clicked', async() => {
+    const mockCloseModal = jest.fn();
+    render(<Modal showContent={true} closeModal={mockCloseModal} children={<div>Modal Content</div>} />);
+    userEvent.click(screen.getByTestId('modal-close-btn'));
+    await waitFor(() => {
+      expect(mockCloseModal).toHaveBeenCalled();
+    })
+  });
+});
