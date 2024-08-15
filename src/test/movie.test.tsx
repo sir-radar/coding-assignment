514d7912f2fa1af50f1ab717b0d36de1b86556bb
Samson Nwokike <nwokikesamson@gmail.com>
2024-08-15 02:21:00 +0100

    fix flaky movie test

--- src/test/movie.test.tsx
+++ src/test/movie.test.tsx
@@ -1,35 +1,30 @@
-import { screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { renderWithProviders } from './utils'
-import App from '../App'
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from './utils';
+import App from '../App';
 
 it('movies starred and saved to watch later', async () => {
-    renderWithProviders(<App />)
+    renderWithProviders(<App />);
 
-    await userEvent.type(screen.getByTestId('search-movies'), 'forrest gump')
-    await waitFor(() => {
-      expect(screen.getAllByText('Through the Eyes of Forrest Gump')[0]).toBeInTheDocument()
-    })
-    const starMovieLink = screen.getAllByTestId('starred-link')[0]
-    await waitFor(() => {
-        expect(starMovieLink).toBeInTheDocument()
-    })
-    await userEvent.click(starMovieLink)
-    await waitFor(() => {
-      expect(screen.getByTestId('star-fill')).toBeInTheDocument()
-    })
-    await waitFor(() => {
-        expect(screen.getByTestId('unstar-link')).toBeInTheDocument()
-    })
+    await userEvent.type(screen.getByTestId('search-movies'), 'forrest gump');
 
-    const watchLaterLink = screen.getAllByTestId('watch-later')[0]
-    await waitFor(() => {
-        expect(watchLaterLink).toBeInTheDocument()
-    })
-    await userEvent.click(watchLaterLink)
-    await waitFor(() => {
-      expect(screen.getByTestId('remove-watch-later')).toBeInTheDocument()
-    })
+    const movieTitle = await screen.findAllByText('Through the Eyes of Forrest Gump');
+    expect(movieTitle[0]).toBeInTheDocument();
 
-    await userEvent.click(screen.getAllByTestId('remove-watch-later')[0])
-})
+    const starMovieLink = screen.getAllByTestId('starred-link')[0];
+    await userEvent.click(starMovieLink);
+
+    expect(await screen.findByTestId('star-fill')).toBeInTheDocument();
+    expect(screen.getByTestId('unstar-link')).toBeInTheDocument();
+
+    const watchLaterLink = screen.getAllByTestId('watch-later')[0];
+    await userEvent.click(watchLaterLink);
+
+    expect(await screen.findByTestId('remove-watch-later')).toBeInTheDocument();
+
+    await userEvent.click(screen.getAllByTestId('remove-watch-later')[0]);
+
+    await waitFor(() => {
+        expect(screen.queryByTestId('remove-watch-later')).not.toBeInTheDocument();
+    });
+});
